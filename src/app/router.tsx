cfe68e69e687b5bdf5a8dd22c7ec03c25794ab45
Diego Mc <projkd1@gmail.com>
2025-01-17 13:52:23 +0200

    Added basic routing

---
+++ src/app/router.tsx
@@ -0,0 +1,15 @@
+import { Navigate, Route, Routes } from 'react-router'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/login" element={<h1>Login</h1>} />
+      {/* TODO: add a layout for wap to be checked for auth */}
+      {/* TODO: add a layout for wap to have an inner slot to only render the different inner content */}
+      <Route path="/wap/foods" element={<h1>Foods</h1>} />
+      <Route path="/wap/cars" element={<h1>Cars</h1>} />
+      <Route path="/wap/products" element={<h1>Products</h1>} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  )
+}
